a798eebc2e8c667a70bcee13f7f3e30225b109ae
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2019-06-06 12:57:33 -0400

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -5,5 +5,10 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.26" // A version or constraint - Ex. "^0.5.0"
+    }
   }
 };
